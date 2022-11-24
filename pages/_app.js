1c3b09dee50ee88e924ea5a60ed2e5c83e6ad56c
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2022-11-24 21:50:30 +0800

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { ChakraProvider } from '@chakra-ui/react'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    )
   }
   
   export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Test from '../components/test'
 export default function Home() {
   const color_scheme=['#3BBA9C','#2E3047','#43455C','#3C3F58','gray.400']
   return (
-    <ChakraProvider>
+    <>
       <Navbar />
       <Box pos = "fixed" bg={color_scheme[1]}
       opacity={'1'} height={'calc(100vh)'} width={'calc(100vw)'} 
@@ -16,6 +16,6 @@ export default function Home() {
         <Threejs/>
       </Box>
       <Contact />
-    </ChakraProvider>
+    </>
   )
 }
