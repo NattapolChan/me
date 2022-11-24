44949eb84470bd8e028f2732557c3ce09c81788d
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2022-11-24 20:04:42 +0800

    test

--- components/Nav.js
+++ components/Nav.js
@@ -18,6 +18,7 @@ import {
 import Link from "next/link";
 import { Link as ChakraLink} from "@chakra-ui/react"
 import React from "react";
+import NAV_ITEMS from './NAV_ITEMS';
 
 
 const ChakraNextLink = React.forwardRef(({ href, children, ...props }, ref) => {
@@ -262,48 +263,3 @@ export default function Navbar() {
         </ChakraNextLink>
     );
   };
-
-
-  
-  const NAV_ITEMS = [
-    {
-      label: 'Research',
-      children: [
-        {
-          label: 'Parallel Curves Detection Method based on Hough Transform',
-          subLabel: 'see page 135',
-          href: 'https://www.math.science.cmu.ac.th/tjcdcggg/Book-abstract.pdf#page=135',
-        },
-        {
-          label: 'Deeply Supervised CNN with Attention Mechanism',
-          subLabel: 'High school graduation project',
-
-        },
-      ],
-    },
-    {
-      label: 'Project',
-      children: [
-        {
-          label: 'SaraTree',
-          subLabel: 'A submission to HACKVCIS hackathon',
-          href: 'https://saratree.vercel.app/'
-        },
-        {
-          label: 'Guitar Tab Generator',
-          subLabel: 'A website to generate guitar tab from .wav file',
-          href: 'https://nattapolguitartab.netlify.app/'
-        },
-      ]
-    },
-    {
-      label: 'Resume',
-    //   href: 'https://docs.google.com/document/d/1GJzsIIE7Eez76El0f2XoZvN4Wolvu9-fxL5yCWRA200/edit?usp=sharing',
-      href: '/Chanpaisit_Nattapol_Resume.pdf',
-    },
-    {
-      label: 'About',
-      href: '/About',
-      // href: '/me/utils/Chanpaisit_Nattapol_Resume.pdf',
-    },
-  ];
---
+++ pages/Research.js
@@ -0,0 +1,60 @@
+import { VStack, Stack, Box, Flex, Icon, LinkIcon, Text } from '@chakra-ui/react'
+import React from 'react'
+import Link from "next/link";
+import { Link as ChakraLink} from "@chakra-ui/react"
+import NAV_ITEMS from '../components/NAV_ITEMS';
+
+
+const ChakraNextLink = React.forwardRef(({ href, children, ...props }, ref) => {
+  return (
+    <Link href={href} passHref>
+      <ChakraLink ref={ref} {...props}>
+        {children}
+      </ChakraLink>
+    </Link>
+  );
+});
+
+const SubNav = ({ label, href, subLabel }) => {
+    const color_scheme=['#3BBA9C','#2E3047','#43455C','#3C3F58','gray.400']
+    const linkColor = color_scheme[4];
+    const linkHoverColor = color_scheme[0];
+    const popoverContentBgColor = color_scheme[1];    
+    return (
+        <Link href={href ? href :'#'}>
+            <Box 
+                unselectable='true'
+                w='50em'
+                pl={'30px'}
+                pr={'30px'}
+                pt={'25px'}
+                pb={'25px'}
+                textAlign='center'
+                borderWidth={'1px'}
+                borderColor={color_scheme[0]}
+                _hover={{
+                  borderColor: linkHoverColor,
+                  color: linkHoverColor
+                }}
+                bgColor={color_scheme[2]}
+                color={linkColor}
+                zIndex='10'
+            >
+                <Text unselectable='on'>{label}</Text>
+            </Box>
+        </Link>
+    );
+  };
+
+export default function Research(){
+    const color_scheme=['#3BBA9C','#2E3047','#43455C','#3C3F58','gray.400']
+    console.log("hi")
+    console.log(NAV_ITEMS[0].children)
+    return (
+        <VStack w='100vw' h='100vh' bg='#2E3047' justifyContent='center' alignItems='center'>
+            {NAV_ITEMS[0].children.map((child) => (
+                <SubNav key={child.label} {...child} />
+            ))}
+        </VStack>
+    )
+}
